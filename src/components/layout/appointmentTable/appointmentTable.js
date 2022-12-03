c8fefc9fd1ddac18f7b9a6b6d7b7add568642038
Chirea Liviu <72073267+lichirea@users.noreply.github.com>
2022-12-03 21:27:45 +0200

    Sorting appointments table (#9)
    * v1
    
    * appointments sorting
    
    * merging
    
    Co-authored-by: Isabela <72067839+Mocanu-Isabela@users.noreply.github.com>

--- src/components/layout/appointmentTable/appointmentTable.js
+++ src/components/layout/appointmentTable/appointmentTable.js
@@ -1,13 +1,6 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import search from "../../../assets/svg/magnifying-glass-solid.svg"
-
 import {
-    TableContainer,
     Table,
-    TableHead,
-    TableRow,
-    TableCell,
+    TableContainer,
     TableBody,
     Paper,
     Button,
@@ -15,9 +8,14 @@ import {
 import pageRight from "../../../assets/svg/arrow-right-solid.svg";
 import pageLeft from "../../../assets/svg/arrow-left-solid.svg";
 import plus from "../../../assets/svg/plus-solid.svg";
+import search from "../../../assets/svg/magnifying-glass-solid.svg";
+
 import { AppointmentTableRow } from "./appointmentTableRow";
+import  TableHeader  from "./appointmentTableHeader";
 
 import styles from "./appointmentTable.module.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function CustomTablePagination({page, totalPages, onPageChange}) {
 
@@ -74,6 +72,31 @@ function CustomTablePagination({page, totalPages, onPageChange}) {
     );
 }
 
+function descendingComparator(a, b, orderBy) {
+    if(b[orderBy] < a[orderBy]){
+        return -1
+    }
+    if(b[orderBy] > a[orderBy]){
+        return 1
+    }
+    return 0
+}
+
+function getComparator(order, orderBy) {
+    return order === "desc"
+        ? (a,b) => descendingComparator(a, b, orderBy)
+        : (a,b) => -descendingComparator(a, b, orderBy)
+}
+
+const sortedRowInformation = (rowArray, comparator) => {
+    const stabilizedRowArray = rowArray.map((el, index) => [el, index])
+    stabilizedRowArray.sort((a,b) => {
+        const order = comparator(a[0], b[0])
+        if(order !== 0) return order
+        return a[1] - b[1]
+    })
+    return stabilizedRowArray.map((el) => el[0])
+}
 
 export function AppointmentTable() {
     const [inputValue, setInputValue] = useState('')
@@ -81,6 +104,24 @@ export function AppointmentTable() {
     const [page, setPage] = useState(1);
     const [totalPages, setTotalPages] = useState(3);
     const [apps, setApps] = useState([]);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleRequestSort = (event, property) => {
+        const isAscending = (valueToOrderBy === property && orderDirection === 'asc')
+        setValueToOrderBy(property)
+        setOrderDirection(isAscending ? 'desc' : 'asc');
+        console.log(isAscending + "  " + property)
+    };
+
+    // const createSortHandler = (property) => (event) => {
+    //     handleRequestSort(event, property)
+    // }
+
+    const [orderDirection, setOrderDirection] = useState('asc');
+    const [valueToOrderBy, setValueToOrderBy] = useState('type');
 
 
     useEffect(() => {
@@ -110,6 +151,7 @@ export function AppointmentTable() {
         setPage(newPage)
         //setPage(newPage);
     };
+
 
     return (
         <div>
@@ -141,27 +183,12 @@ export function AppointmentTable() {
             
             <TableContainer component={Paper} >
                 <Table sx={{ minWidth: 1200 }} aria-label="simple table">
-                    <TableHead>
-                        <TableRow
-                            sx={{
-                                '& .MuiTableCell-root': {
-                                    color: 'white',
-                                    backgroundColor: '#2785FF'
-                                }
-                            }}>
-                            <TableCell>Type</TableCell>
-                            <TableCell>Date</TableCell>
-                            <TableCell>Time</TableCell>
-                            <TableCell>Doctor</TableCell>
-                            <TableCell>Patient</TableCell>
-                            <TableCell
-                                sx={{
-                                    width: "100px",
-                                }}>
-
-                            </TableCell>
-                        </TableRow>
-                    </TableHead>
+                    <TableHeader
+                        valueToOrderBy={valueToOrderBy}
+                        orderDirection={orderDirection}
+                        handleRequestSort={handleRequestSort}
+                        />
+ 
                     <TableBody>
                         {apps?.map((app) => (
                             <AppointmentTableRow key={app.id_appointment} app={app} getA={getAppointments}/>
