f41616e4869646dc41205c5667da2fe807cfa8bc
Chirea Liviu <72073267+lichirea@users.noreply.github.com>
2022-12-20 23:35:26 +0200

    Refactor doctors table (#12)
    * woof woof
    
    * de ce e asa grea tema la VR? :(
    
    * pam pam boom boom
    
    * merging
    
    Co-authored-by: Isabela <72067839+Mocanu-Isabela@users.noreply.github.com>

---
+++ src/components/layout/doctorTable/doctorTable.js
@@ -0,0 +1,166 @@
+import {
+    Table,
+    TableContainer,
+    TableBody,
+    Paper,
+    Button,
+} from "@mui/material";
+import pageRight from "../../../assets/svg/arrow-right-solid.svg";
+import pageLeft from "../../../assets/svg/arrow-left-solid.svg";
+import plus from "../../../assets/svg/plus-solid.svg";
+import search from "../../../assets/svg/magnifying-glass-solid.svg";
+
+import { DoctorTableRow } from "./doctorTableRow";
+import  TableHeader  from "./doctorTableHeader";
+
+import styles from "./doctorTable.module.css";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function CustomTablePagination({page, totalPages, onPageChange}) {
+
+    const handleBackButtonClick = (event) => {
+        onPageChange(page - 1);
+    };
+
+    const handleNextButtonClick = () => {
+        onPageChange(page + 1);
+    };
+
+
+    return (
+        <div className={` ${styles["paginationBtnGroup"]} 
+        d-flex flex-row align-content-center justify-content-end w-100`}>
+            <Button
+                className="mt-3"
+                sx={{
+                    backgroundColor: 'white',
+                    boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
+                    width: '30px',
+                    minWidth: '30px',
+                    height: '30px',
+                    minHeight: '30px'
+                }}
+                onClick={handleBackButtonClick}
+                disabled={page === 1}
+                classes={{ disabled: styles.disabledPagination }}
+            >
+                <img src={pageLeft} alt="previous page" className={`h-100 ${styles["buttonImg"]}`}></img>
+            </Button>
+            <div className={`${styles['pageNumberText']}`}>
+                {page} of {totalPages}
+            </div>
+            <Button
+                className="mt-3"
+                sx={{
+                    backgroundColor: 'white',
+                    boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
+                    width: '30px',
+                    minWidth: '30px',
+                    height: '30px',
+                    minHeight: '30px'
+                }}
+                onClick={handleNextButtonClick}
+                disabled={page === totalPages}
+                classes={{ disabled: styles.disabledPagination }}
+            >
+                <img src={pageRight} alt="next page" className={`h-100 ${styles["buttonImg"]}`}></img>
+            </Button>
+        </div>
+    );
+}
+
+export function DoctorTable() {
+    const [searchTerm, setSearchTerm] = useState('')
+    const [timer, setTimer] = useState(null)
+    const [page, setPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(3);
+    const [apps, setApps] = useState([]);
+    const [orderDirection, setOrderDirection] = useState('asc');
+    const [valueToOrderBy, setValueToOrderBy] = useState('name');
+
+    const handleRequestSort = (event, property) => {
+        const isAscending = (valueToOrderBy === property && orderDirection === 'asc')
+        setValueToOrderBy(property)
+        setOrderDirection(isAscending ? 'desc' : 'asc')
+        setPage(1)
+    };
+
+    useEffect(() => {
+        getDoctors();
+    }, [page, orderDirection, valueToOrderBy, searchTerm]);
+
+
+    const getDoctors = () => {
+        fetch(`http://localhost:5000/doctor/` +
+        `?page=${page}`)
+            .then(response => response.json())
+            .then(response => {
+                setTotalPages(response.meta.pages)
+                setApps(response.data);
+            });
+    }
+
+    const inputChanged = e => {
+        clearTimeout(timer)
+
+        const newTimer = setTimeout(() => {
+            setPage(1)
+            setSearchTerm(e.target.value)
+        }, 500)
+
+        setTimer(newTimer)
+    }
+
+    const onPageChange = (newPage) => {
+        setPage(newPage)
+    };
+
+
+    return (
+        <div>
+            <div className="d-flex">
+                <div className="d-flex col-6">
+                    <div className={`${styles['search-box']} align-self-end`}>
+                        <input className={`${styles['search-text']}`} type="text" onChange={inputChanged} placeholder="Search..."/>
+                        <img src={search} alt="search" className={`h-100 ${styles["buttonImg"]}`}></img>
+                    </div>
+                </div>
+                <div className="d-flex flex-row-reverse justify-content-between col-6">
+                    <Link to="/doctors/create">
+                        <Button
+                            className={`${styles["rowBtn"]} mb-2`}
+                            sx={{
+                                backgroundColor: '#2785FF',
+                                boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
+                                width: '50px',
+                                minWidth: '30px',
+                                height: '30px',
+                                minHeight: '30px'
+                            }}
+                        >
+                            <img src={plus} alt="add" className={`h-100 ${styles["buttonImg"]}`}></img>
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+            
+            <TableContainer component={Paper} >
+                <Table sx={{ minWidth: 1200 }} aria-label="simple table">
+                    <TableHeader
+                        valueToOrderBy={valueToOrderBy}
+                        orderDirection={orderDirection}
+                        handleRequestSort={handleRequestSort}
+                        />
+ 
+                    <TableBody>
+                        {apps?.map((app) => (
+                            <DoctorTableRow key={app.id_doctor} app={app} getA={getDoctors}/>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <CustomTablePagination page={page} totalPages={totalPages} onPageChange={onPageChange}/>
+        </div>
+    );
+}
---
+++ src/components/layout/doctorTable/doctorTableRow.js
@@ -0,0 +1,98 @@
+import { TableRow, TableCell, Button } from "@mui/material";
+import select from "../../../assets/svg/select-svgrepo-com.svg";
+import styles from "./doctorTable.module.css";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Modal, OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+const selectDoctor = async (doctorId, getA) => {
+    const status = new Promise((resolve) => {
+        axios
+            .select(`http://localhost:5000/doctors/${doctorId}`)
+            .catch((error) => {
+                console.log(error);
+                resolve(false);
+            })
+            .then(() => {
+                getA();
+            })
+    });
+    const result = await status;
+    return result;
+};
+
+export const DoctorTableRow = ({ app, getA }) => {
+    const [show, setShow] = useState(false);
+    const [showAdd, setShowAdd] = useState(false);
+
+    const showHandler = () => {
+        setShow(true);
+    }
+
+    const closeHandler = () => {
+        setShow(false);
+    }
+
+    const showAddHandler = () => {
+        setShowAdd(true);
+    }
+
+    const closeAddHandler = () => {
+        setShowAdd(false);
+    }
+
+    useEffect(() => {
+        closeHandler();
+        closeAddHandler();
+    }, [app]);
+
+    const selectHandler = async (e) => {
+        const response = await selectDoctor(app.id_doctor, getA);
+    }
+
+    return (
+        <TableRow
+            key={app.id_doctor}
+            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        >
+            <TableCell component="th" scope="row">
+                {app.first_name} {app.last_name}
+            </TableCell>
+            <TableCell>{app.specialization}</TableCell>
+            <TableCell>{app.hospital}</TableCell>
+            <TableCell>{app.position}</TableCell>
+            <TableCell
+                sx={{
+                    width: "100px",
+                    display: "flex",
+                    gap: "10px",
+                    justifyContent: "right"
+                }}>
+                <OverlayTrigger
+                    overlay={
+                        <Tooltip id={`tooltip-top`}>
+                            Select
+                        </Tooltip>
+                    }
+                >
+                    <Button
+                        className={`${styles["rowBtn"]}`}
+                        sx={{
+                            backgroundColor: '#2785FF',
+                            boxShadow: '0px 8px 15px rgba(0, 0, 0, 0.1)',
+                            width: '30px',
+                            minWidth: '30px',
+                            height: '30px',
+                            minHeight: '30px'
+                        }}
+                        onClick={selectHandler}
+                    >
+                        <img src={select} alt="select" className={`h-100 ${styles["buttonImg"]}`}></img>
+                    </Button>
+                </OverlayTrigger>
+
+            </TableCell>
+        </TableRow >
+    );
+}
